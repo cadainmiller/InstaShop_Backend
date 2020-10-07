3466b1479006e899ddee5dbc5261a648a5038be3
Wild Mobile <cadainmiller@gmail.com>
2020-10-06 19:48:49 -0500

    Added Swagger to project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 4000;
 const morgan = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
 const app = express();
 
 const userRoutes = require("./routes/userRoute"); //bring in our user routes
@@ -10,11 +11,12 @@ const productRoutes = require("./routes/productRoute"); //bring in our product r
 const invoiceRoutes = require("./routes/invoiceRoute"); //bring in our invoice routes
 const orderRoutes = require("./routes/orderRoute"); //bring in our order routes
 const uploadRoutes = require("./routes/uploadRoutes"); //bring in our upload routes
+swaggerDocument = require("./swagger.json");
 
 app.use(cors()); // configure cors
 //configure body parser
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // Make "public" Folder Publicly Available
 app.use("/public", express.static("public"));
@@ -27,16 +29,16 @@ app.use(morgan("dev")); // configire morgan
 require("./config/db")(app);
 //db config ends here
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // define first route
 app.get("/api", (req, res) => {
-  res.send("Working Ya")
+  res.send("Working Ya");
 });
 
 // define first route
 app.get("/", (req, res) => {
-  res.send("API LIVE")
+  res.send("API LIVE");
 });
-
 
 app.use("/user", userRoutes);
 app.use("/product", productRoutes);
